d5aa58e6e1c3f996f66f9931b39982a0d3e4ed58
Yuqi Yang <ytsyang@ucdavis.edu>
2014-07-04 12:55:20 -0700

    Initial Commit

---
+++ TestED/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  TestED
+//
+//  Created by Young on 07-04－此月第1周.
+//  Copyright (c) 2014年 Young. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
